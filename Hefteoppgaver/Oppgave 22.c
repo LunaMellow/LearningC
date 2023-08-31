591a044ed4190167977b491ea7b6fea147508bde
LunaMellow <nore0404@gmail.com>
2023-08-31 12:02:39 +0200

    In progress

--- Hefteoppgaver/Oppgave 22.c
+++ Hefteoppgaver/Oppgave 22.c
@@ -7,10 +7,11 @@
  *          - This is in in the program
  *
  *      @file Oppgave 22.c
- *      @date Oppgave 22.c
+ *      @date 8/31/2023
  *
- *      @author lunap
+ *      @author Luna S.
  *      @alias LunaMellow
  */
 
 // Includes
+
