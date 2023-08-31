4c82eee5168dc4d41ce922e5b7ef47d08be091ce
LunaMellow <nore0404@gmail.com>
2023-08-31 11:59:54 +0200

    Started Task

---
+++ Hefteoppgaver/Oppgave 22.c
@@ -0,0 +1,16 @@
+
+/**     Project name
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oppgave 22.c
+ *      @date Oppgave 22.c
+ *
+ *      @author lunap
+ *      @alias LunaMellow
+ */
+
+// Includes
