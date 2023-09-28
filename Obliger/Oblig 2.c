0e7d8ae3838b762132b1551d622c8afaffeb5525
LunaMellow <nore0404@gmail.com>
2023-09-28 23:22:12 +0200

    In Progress

---
+++ Obliger/Oblig 2.c
@@ -0,0 +1,42 @@
+
+/**     Project name
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oblig 2.c
+ *      @date 28/09/23
+ *
+ *      @author Luna Sofie Bergh
+ *      @alias LunaMellow
+ */
+
+// Includes
+#include <stdio.h>
+#include <stdbool.h>
+
+// Declare consts
+const int MAXOPERASJONER = 50;
+
+/**
+ *      Main Program starts here
+ */
+
+int main() {
+
+    // Declare variables
+    float tallInput[MAXOPERASJONER];
+    float akkumulator = 0.0;
+    float input;
+
+    while (1) {
+
+        // Print first variable
+        printf("~ %f ", akkumulator);
+
+
+    }
+    return 0;
+}
