856c483a02e46eb49506e74dda4b0d1d29fce08c
LunaMellow <nore0404@gmail.com>
2023-11-06 12:49:28 +0100

    In Progress - Stable, empty functions

--- Obliger/Oblig 3.5.c
+++ Obliger/Oblig 3.5.c
@@ -50,11 +50,11 @@ int main ()  {
 
     while (kommando != 'Q')  {
         switch (kommando)  {
-            case 'O':  oversikt();      break;    //  Oversikt over varelager.
-            case 'L':  ledige();        break;    //  Registrer ny vare.
-            case 'U':  utlaan();        break;    // Motta 'x' stk. av vare.
-            case 'I':  innlevering();   break;    // Selg 'x' stk. av vare.
-            case 'F':  finnLaaner();    break;    //  Fjern en vare fra lager.
+            case 'O':  oversikt();      break;    //  Oversikt over utlånte traller
+            case 'L':  ledige();        break;    //  Oversikt over ledige traller
+            case 'U':  utlaan();        break;    //  Utlån av tralle
+            case 'I':  innlevering();   break;    //  Lever inn tralle
+            case 'F':  finnLaaner();    break;    //  Finn tralle
             default:   skrivMeny();     break;    //  Ikke-eksisterende menyvalg.
         }
         kommando = lesKommando();
@@ -95,6 +95,36 @@ char lesKommando() {
     printf("\nSkriv kommando:  ");
     scanf(" %c", &tegn);   getchar();
     return (toupper(tegn));
+}
+
+/**
+ *  Reads and returns a number between two given bounds
+ *
+ *  @param   tekst  - Text for the user to input a number
+ *  @param   min    - Minimum for the input and accepted numerical value
+ *  @param   max    - Minimum for the input and accepted numerical value
+ *
+ *  @return  Accepted value in the range 'min' - 'max'
+ */
+int lesTall(const char tekst[], const int min, const int max) {
+    int tall;
+    do  {
+        printf("\t%s (%i-%i):  ", tekst, min, max);
+        scanf("%i", &tall);     getchar();
+    } while (tall < min  ||  tall > max);
+    return tall;
+}
+
+/**
+ *  Reads text into the provided char array/string.
+ *
+ *  @param   ledetekst  - The prompt or message displayed to the user, indicating what text to enter
+ *  @param   tekst      - Char array filled with the user inputted text
+ *
+ */
+void lesTekst(const char ledetekst[], char tekst[]) {
+    printf("\t%s:  ", ledetekst);
+    gets(tekst);                    //  Updates ORIGINAL/actual parameter.
 }
 
 /**
