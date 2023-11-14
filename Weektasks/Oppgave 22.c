2ab0c8ea461f1954f2ab9becbf168246a2baaec6
Luna Sofie <nore0404@gmail.com>
2023-11-14 18:26:41 +0100

    In Progress

---
+++ Weektasks/Oppgave 22.c
@@ -0,0 +1,17 @@
+
+/**     Project name
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oppgave 22.c
+ *      @date 8/31/2023
+ *
+ *      @author Luna S.
+ *      @alias LunaMellow
+ */
+
+// Includes
+
