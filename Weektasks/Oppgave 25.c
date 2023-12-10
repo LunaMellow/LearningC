5121d98a110f66cf5906324fd425b9ca68b6b4e9
Luna Sofie <nore0404@gmail.com>
2023-12-10 16:21:54 +0100

    In Progress -> Unfinished functions

---
+++ Weektasks/Oppgave 25.c
@@ -0,0 +1,55 @@
+
+/**     Recreating String.h lol :p
+ *      Task 25 in grprog.pdf
+ *
+ *      Program description:
+ *          - This is in in the program
+ *          - This is in in the program
+ *          - This is in in the program
+ *
+ *      @file Oppgave 25.c
+ *      @date 09-Dec-23
+ *
+ *      @author Luna S.
+ *      @alias LunaMellow
+ */
+
+// Includes
+#include <stdio.h>
+
+// Declare functions
+int mstrlen(const char s[]);
+void mstrcpy(char s[], const char t[]);
+void mstrcat(char s[], const char t[]);
+int mstrcmp(const char s[], const char t[]);
+
+/**
+ *      Main Program starts here
+ */
+int main() {
+
+     return 0;
+}
+
+int mstrlen(const char s[]) {
+    int len = 0;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        len++;
+    }
+
+    return len;
+}
+
+void mstrcpy(char s[], const char t[]) {
+    
+
+}
+
+void mstrcat(char s[], const char t[]) {
+
+}
+
+int mstrcmp(const char s[], const char t[]) {
+
+}
